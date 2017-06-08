5e25ee549e737670b3354eba8f0e21c8442b3638
MikhailTodes <mikhailtodes2016@u.northwestern.edu>
2017-06-08 16:47:49 -0500

    Updated pi instal script

--- pi_installations.sh
+++ pi_installations.sh
@@ -3,7 +3,6 @@
 #Basic updates and upgrades with automatic 'yes' response for installation
 sudo apt-get update
 yes Y | sudo apt-get upgrade
-yes Y | sudo rpi-update
 
 #Add the auto complete ignore case line to the bashrc and the l alias
 echo "alias ll='ls -alF'" >> ~/.bashrc
@@ -11,6 +10,9 @@ echo "alias la='ls -A'" >> ~/.bashrc
 echo "alias l='ls -CF'" >> ~/.bashrc
 
 echo "bind 'set completion-ignore-case on'" >> ~/.bashrc
+
+#Add line to the .profile so Pi updates time (Google) after loss of power
+echo "sudo sntp -s time.google.com" >> ~/.profile
 
 #Emacs
 yes Y | sudo apt-get install Emacs
@@ -25,22 +27,29 @@ yes Y | sudo apt-get install python-pip python3-pip
 yes Y | sudo apt-get install build-essential cmake pkg-config
 
 #Image Processors
-yes Y | sudo apt-get install libjpeg-dev
+yes Y | sudo apt-get install libjpeg-dev libjpeg8-dev
 yes Y | sudo apt-get install libtiff5-dev libjasper-dev libpng12-dev
 
 #Video Processors
 yes Y | sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+yes Y | sudo apt-get install libxvidcore-dev libx264-dev
 
 #Gui Stuff and other packages used in OpenCV
-yes Y | sudo apt-get install libgtk2.0-dev libatlas-base-dev gfortran
+yes Y | sudo apt-get install libgtk2.0-dev libgtk-3-dev libatlas-base-dev gfortran
 
-#Download and install precompiled OpenCV
-wget "https://github.com/MikhailTodes/OpenCV-for-Pi/raw/master/latest-OpenCV.deb"
-sudo dpkg -i latest-OpenCV.deb
+#Download and install OpenCV
+yes Y | sudo apt-get install python-opencv
+
+#Make sure the 3.5mm jack is used as audio out
+echo "sudo amixer cset numid=3 1" >> ~/.profile
+sudo amixer cset numid=3 1
+
+#mplayer for audio out
+yes Y | sudo apt-get install mplayer
 
 #Speech Recognition
 yes Y | sudo apt-get install flac
-yes Y | sudo apt-get install python-pyaudio python3-pyaudio sox
+yes Y | sudo apt-get install sox
 sudo pip install SpeechRecognition
 sudo pip3 install SpeechRecognition
 yes Y | sudo apt-get install portaudio19-dev python-all-dev python3-all-dev
